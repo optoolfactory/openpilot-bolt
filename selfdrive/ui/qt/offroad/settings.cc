745fce6a628354f2598db70d175e12166dd426c4
whrho <whrho@sparrowfasoo.com>
2021-09-23 17:38:04 +0900

    fixed for QCOM only buttons

--- selfdrive/ui/qt/offroad/settings.cc
+++ selfdrive/ui/qt/offroad/settings.cc
@@ -192,6 +192,9 @@ DevicePanel::DevicePanel(QWidget* parent) : QWidget(parent) {
       Hardware::reboot();
     }
   });
+
+
+#ifdef QCOM
   QPushButton *reboot_rmprebuilt_btn = new QPushButton("빌드부팅");
   power_layout->addWidget(reboot_rmprebuilt_btn);
   QObject::connect(reboot_rmprebuilt_btn, &QPushButton::clicked, [=]() {
@@ -200,7 +203,6 @@ DevicePanel::DevicePanel(QWidget* parent) : QWidget(parent) {
       }
   });
 
-#ifdef QCOM
   QPushButton *cleanbuild_btn = new QPushButton("클린 빌드부팅");
   power_layout->addWidget(cleanbuild_btn);
   QObject::connect(cleanbuild_btn, &QPushButton::clicked, [=]() {
