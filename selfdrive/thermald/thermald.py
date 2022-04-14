ee43ede9d126aaed6398f5615c2b7bb25e5e5543
optoolfactory <80941254+optoolfactory@users.noreply.github.com>
2022-04-14 16:31:54 +0900

    Update thermald.py

--- selfdrive/thermald/thermald.py
+++ selfdrive/thermald/thermald.py
@@ -330,38 +330,38 @@ def thermald_thread():
     set_offroad_alert_if_changed("Offroad_InvalidTime", (not startup_conditions["time_valid"]))
 
     # Show update prompt
-    try:
-      last_update = datetime.datetime.fromisoformat(params.get("LastUpdateTime", encoding='utf8'))
-    except (TypeError, ValueError):
-      last_update = now
-    dt = now - last_update
+    # try:
+    #  last_update = datetime.datetime.fromisoformat(params.get("LastUpdateTime", encoding='utf8'))
+    #except (TypeError, ValueError):
+    #  last_update = now
+    #dt = now - last_update
 
-    update_failed_count = params.get("UpdateFailedCount")
-    update_failed_count = 0 if update_failed_count is None else int(update_failed_count)
-    last_update_exception = params.get("LastUpdateException", encoding='utf8')
+    #update_failed_count = params.get("UpdateFailedCount")
+    #update_failed_count = 0 if update_failed_count is None else int(update_failed_count)
+    #last_update_exception = params.get("LastUpdateException", encoding='utf8')
 
-    if update_failed_count > 15 and last_update_exception is not None:
-      if tested_branch:
-        extra_text = "Ensure the software is correctly installed"
-      else:
-        extra_text = last_update_exception
+    #if update_failed_count > 15 and last_update_exception is not None:
+    #  if tested_branch:
+    #    extra_text = "Ensure the software is correctly installed"
+    #  else:
+    #    extra_text = last_update_exception
 
-      set_offroad_alert_if_changed("Offroad_ConnectivityNeeded", False)
-      set_offroad_alert_if_changed("Offroad_ConnectivityNeededPrompt", False)
-      set_offroad_alert_if_changed("Offroad_UpdateFailed", True, extra_text=extra_text)
-    elif dt.days > DAYS_NO_CONNECTIVITY_MAX and update_failed_count > 1:
-      set_offroad_alert_if_changed("Offroad_UpdateFailed", False)
-      set_offroad_alert_if_changed("Offroad_ConnectivityNeededPrompt", False)
-      set_offroad_alert_if_changed("Offroad_ConnectivityNeeded", True)
-    elif dt.days > DAYS_NO_CONNECTIVITY_PROMPT:
-      remaining_time = str(max(DAYS_NO_CONNECTIVITY_MAX - dt.days, 0))
-      set_offroad_alert_if_changed("Offroad_UpdateFailed", False)
-      set_offroad_alert_if_changed("Offroad_ConnectivityNeeded", False)
-      set_offroad_alert_if_changed("Offroad_ConnectivityNeededPrompt", True, extra_text=f"{remaining_time} days.")
-    else:
-      set_offroad_alert_if_changed("Offroad_UpdateFailed", False)
-      set_offroad_alert_if_changed("Offroad_ConnectivityNeeded", False)
-      set_offroad_alert_if_changed("Offroad_ConnectivityNeededPrompt", False)
+    #  set_offroad_alert_if_changed("Offroad_ConnectivityNeeded", False)
+    #  set_offroad_alert_if_changed("Offroad_ConnectivityNeededPrompt", False)
+    #  set_offroad_alert_if_changed("Offroad_UpdateFailed", True, extra_text=extra_text)
+    #elif dt.days > DAYS_NO_CONNECTIVITY_MAX and update_failed_count > 1:
+    #  set_offroad_alert_if_changed("Offroad_UpdateFailed", False)
+    #  set_offroad_alert_if_changed("Offroad_ConnectivityNeededPrompt", False)
+    #  set_offroad_alert_if_changed("Offroad_ConnectivityNeeded", True)
+    #elif dt.days > DAYS_NO_CONNECTIVITY_PROMPT:
+    #  remaining_time = str(max(DAYS_NO_CONNECTIVITY_MAX - dt.days, 0))
+    #  set_offroad_alert_if_changed("Offroad_UpdateFailed", False)
+    #  set_offroad_alert_if_changed("Offroad_ConnectivityNeeded", False)
+    #  set_offroad_alert_if_changed("Offroad_ConnectivityNeededPrompt", True, extra_text=f"{remaining_time} days.")
+    #else:
+    #  set_offroad_alert_if_changed("Offroad_UpdateFailed", False)
+    #  set_offroad_alert_if_changed("Offroad_ConnectivityNeeded", False)
+    #  set_offroad_alert_if_changed("Offroad_ConnectivityNeededPrompt", False)
 
     #startup_conditions["up_to_date"] = params.get("Offroad_ConnectivityNeeded") is None or params.get_bool("DisableUpdates")
     startup_conditions["not_uninstalling"] = not params.get_bool("DoUninstall")
